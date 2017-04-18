a7560fcf24a6b5a00ba178135551bb89684a0e90
Dave Brosius <dbrosius@mebigfatguy.com>
2017-04-17 20:30:20 -0400

    initial commit

---
+++ src/main/java/com/mebigfatguy/quiescence/Quiescence.java
@@ -0,0 +1,52 @@
+package com.mebigfatguy.quiescence;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.tools.ant.BuildListener;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.Task;
+import org.apache.tools.ant.property.LocalProperties;
+
+public class Quiescence extends Task {
+
+    private List<Task> nestedTasks = new ArrayList<>();
+
+    public void addTask(Task nestedTask) {
+        nestedTasks.add(nestedTask);
+    }
+
+    @Override
+    public void execute() {
+        LocalProperties localProperties = LocalProperties.get(getProject());
+        localProperties.enterScope();
+
+        List<BuildListener> savedListeners = removeListeners();
+        try {
+
+            for (Task nestedTask : nestedTasks) {
+                nestedTask.perform();
+            }
+        } finally {
+            restoreListeners(savedListeners);
+            localProperties.exitScope();
+        }
+    }
+
+    private List<BuildListener> removeListeners() {
+        Project p = getProject();
+        List<BuildListener> listeners = p.getBuildListeners();
+        for (BuildListener listener : listeners) {
+            p.removeBuildListener(listener);
+        }
+
+        return listeners;
+    }
+
+    private void restoreListeners(List<BuildListener> savedListeners) {
+        Project p = getProject();
+        for (BuildListener listener : savedListeners) {
+            p.addBuildListener(listener);
+        }
+    }
+}
